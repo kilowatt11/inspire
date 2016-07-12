118322b24fc5f43667f47c2537dc32a0118f28ac
Kurt Hammes <kilowatt11@cableone.net>
2016-07-12 09:25:36 -0600

    time converstion still buggy. should be fixed.

--- app/init.js
+++ app/init.js
@@ -1,4 +1,5 @@
 function startTime() {
+    debugger;
     var today = new Date();
     var h = normalTime(today.getHours());
     var m = today.getMinutes();
@@ -10,16 +11,18 @@ function startTime() {
     var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  
+    if (i < 10) {i = 0 + i};  
     return i;
 }
 function normalTime(time){
+    debugger;
     if(time > 13){
         return time - 12;
     }
-    if(time === 0){
-        return time + 12
-    }
+    return time;
+    // if(time <= 0){
+    //     return time + 12
+    // }
 }
 startTime();
 
