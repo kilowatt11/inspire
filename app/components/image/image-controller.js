023b99661b4d7e8b8db6f151d93aacf9d3392adc
Kurt Hammes <kilowatt11@cableone.net>
2016-07-15 12:03:28 -0600

    fixed image getter

--- app/components/image/image-controller.js
+++ app/components/image/image-controller.js
@@ -2,6 +2,7 @@
 	var image = new ImageService();
 	image.getImage(function(image){
 		console.log(image);
+		image = JSON.parse(image);
 	$('#body').css({'background-image': `url(${image.url})`, 'background-size':'cover','background-repeat': 'no-repeat'});
 	})
 
